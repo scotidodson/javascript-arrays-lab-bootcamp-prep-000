2b1273552339f1e66ab20361324cf25800e42d95
Scoti Dodson <scotidodson@gmail.com>
2018-07-02 22:38:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens.slice(0, kittens-length - 1);
+  var newArray = [kittens.slice(0, kittens-length - 1)];
   return newArray;
 }
 
